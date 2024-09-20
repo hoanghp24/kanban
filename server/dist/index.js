7557e624474d1f19c7a186d78b3387bc8c799b4c
HOANG Pham Huy <Hoang.Pham@action-composites.com.vn>
2024-09-20 08:09:37 +0700

    update server

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,3 +1,40 @@
 "use strict";
-console.log("Hello");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+dotenv_1.default.config();
+const PORT = process.env.PORT || 3001;
+const dbURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lhmx4.mongodb.net/`;
+const app = (0, express_1.default)();
+const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield mongoose_1.default.connect(dbURL);
+        console.log(`Connect to db successfully!!!`);
+    }
+    catch (error) {
+        console.log(`Can not connect to db ${error}`);
+    }
+});
+connectDB()
+    .then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is stating at http://localhost:${PORT}`);
+    });
+})
+    .catch((error) => {
+    console.log(error);
+});
 //# sourceMappingURL=index.js.map
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,30 @@
 import { error } from "console";
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
-const URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lhmx4.mongodb.net/`;
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is starting at http://localhost:${PORT}`);
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.DB_URL}`);
+
+    console.log(`Connect to db successfully!!!`);
+  } catch (error) {
+    console.log(`Can not connect to db ${error}`);
+  }
+};
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is stating at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
